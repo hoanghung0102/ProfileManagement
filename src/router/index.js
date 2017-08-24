5e8ae607669145419780211bc8205b58b3a43585
Hung Hoang <hung.hoang@axonactive.com>
2017-08-24 14:57:40 +0700

    The first commit for Profile Management

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ProfileManagement from '@/components/ProfileManagement'
+import Profile from '@/components/Profile'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'ProfileManagement',
+      component: ProfileManagement
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    }
+  ]
+})
